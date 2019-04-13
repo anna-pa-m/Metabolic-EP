f76d93aa99ac943bf3353a0a602514c57c56137a
Andrea Pagnani <andrea.pagnani@gmail.com>
2019-04-13 09:16:56 +0200

    lower bound error corrected

--- src/ep.jl
+++ src/ep.jl
@@ -75,7 +75,7 @@ function prepareinput(K,Y,lb,ub,beta,verbose,solution,expval)
 
     M,N = size(K)
     M < N || @warn("M = $M ≥ N = $N")
-    all(lb .< ub) || error("lower bound fluxes > upper bound fluxes. Consider swapping lower and upper bounds") 
+    all(lb .<= ub) || error("lower bound fluxes > upper bound fluxes. Consider swapping lower and upper bounds") 
 
     verbose && println(stderr, "Analyzing a $M x $N stoichiometric matrix.")
 
