27577275057d3fdaf6f2972a9ebc991666a054a0
Andrea Pagnani <andrea.pagnani@gmail.com>
2020-06-15 18:47:36 +0200

    added docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,12 @@
 using Documenter, MetabolicEP
 
-push!(LOAD_PATH,"../src/")
-makedocs(sitename = "",
-	 modules = [MetabolicEP],
-	 doctest = true)
+push!(LOAD_PATH, "../src/")
+makedocs(
+    sitename = "",
+    modules = [MetabolicEP],
+    doctest = false)
 deploydocs(
-	   branch = "gh-pages",
-	   repo = "github.com/anna-pa-m/Metabolic-EP",
-	   versions = ["stable" => "v^"]
-	  )
+    branch = "gh-pages",
+    repo = "github.com/anna-pa-m/Metabolic-EP",
+    versions = ["stable" => "v^"],
+)
